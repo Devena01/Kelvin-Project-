7141af79e0085d773ef18ab6852d245c5014ce80
Devena01 <132521746+Devena01@users.noreply.github.com>
2023-05-03 20:36:56 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+// Declaring variable kelvin as 293 degrees
+const kelvin = 293;
+console.log(kelvin);
+// converted kelvin to celsius : celsius is 273.15 degrees less than kelvin
+var difference = (293 - 273.15);
+console.log(difference);
+var celsius = 19.85;
+console.log (celsius);
+// converted 19.85 degrees celsius to Fahrenheit using the formula f= celsius * 9/5 + 32
+var fahrenheit = ((19.85 * 9)/5)+32;
+console.log(fahrenheit);
+// used math.floor to round down 67.73F to 67F
+var fahrenheit= 67;
+console.log(`The temperature is ${fahrenheit} degrees fahrenheit`);
